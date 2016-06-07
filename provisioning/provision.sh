0e2c6476d4debe93a1fdbb0897cfb9ee93b9b8cb
Robert Hafner <tedivm@tedivm.com>
2016-06-07 00:04:05 -0700

    Agnostic directory support, lots of needed dev packages, run from /tmp

--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+pwd
+
 apt_quiet_install () {
    echo "Install package $1"
    apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -f install $1
@@ -18,6 +22,7 @@ apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold
 
 
 # Install Oracle Java
+cd /tmp
 wget -nv --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u92-b14/jdk-8u92-linux-x64.tar.gz
 mkdir /opt/jdk
 tar -zxf jdk-8u92-linux-x64.tar.gz -C /opt/jdk
@@ -27,7 +32,7 @@ update-alternatives --install /usr/bin/javac javac /opt/jdk/jdk1.8.0_92/bin/java
 
 # Install ElasticSearch
 apt_quiet_install elasticsearch
-cp /vagrant/provisioning/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+cp $DIR/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 update-rc.d elasticsearch defaults 95 10
 service elasticsearch start
 
@@ -35,7 +40,7 @@ service elasticsearch start
 # Install Kibana
 apt_quiet_install kibana
 mkdir /etc/kibana
-cp /vagrant/provisioning/kibana.yml /etc/kibana/kibana.yml
+cp $DIR/kibana.yml /etc/kibana/kibana.yml
 update-rc.d kibana defaults 96 9
 service kibana start
 
@@ -43,19 +48,24 @@ service kibana start
 # nginx
 apt_quiet_install nginx
 apt_quiet_install apache2-utils
-echo '' | htpasswd -i -c /etc/nginx/htpasswd.users kibanaadmin
-cp /vagrant/provisioning/default /etc/nginx/sites-available/default
+echo 'screepsstats' | htpasswd -i -c /etc/nginx/htpasswd.users kibanaadmin
+cp $DIR/default /etc/nginx/sites-available/default
 service nginx restart
 
 
+# Install Development Tools
 apt_quiet_install git
+apt_quiet_install python-dev
+apt_quiet_install libxml2-dev
+apt_quiet_install libxslt-dev
 apt_quiet_install python-pip
-
+apt_quiet_install libyaml-dev
 pip install virtualenv
 
 
-cd /vagrant
+# Activate VirtualEnvironment and install dependencies
+cd $DIR/../
 virtualenv env
-source /vagrant/env/bin/activate
-pip install -r requirements.txt
+source ./env/bin/activate
+yes w | pip install --upgrade -r requirements.txt
 

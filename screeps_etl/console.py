371b972db8a3344e533d3b970363732e41ec8461
Robert Hafner <tedivm@tedivm.com>
2016-06-07 00:07:05 -0700

    Persist websocket data to elasticsearch

--- screeps_etl/console.py
+++ screeps_etl/console.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
+from datetime import datetime
+from elasticsearch import Elasticsearch
 import getopt
-
 import screepsapi
 from settings import getSettings
 import sys
@@ -11,25 +12,53 @@ import warnings; warnings.filterwarnings('ignore', message='.*true sslcontext ob
 
 class ScreepsConsole(screepsapi.Socket):
 
+    es = Elasticsearch()
+
     def set_subscriptions(self):
         self.subscribe_user('console')
         self.subscribe_user('cpu')
 
     def process_log(self, ws, message):
-        print message
+        body = {
+            'timestamp': datetime.now(),
+            'message': message
+        }
+        res = self.es.index(index="screeps-console", doc_type="log", body=body)
+        print res
 
     def process_results(self, ws, message):
-        print message
+        body = {
+            'timestamp': datetime.now(),
+            'message': message
+        }
+        res = self.es.index(index="screeps-console", doc_type="result", body=body)
+        print res
+
 
     def process_error(self, ws, message):
-        print message
+        body = {
+            'timestamp': datetime.now(),
+            'message': message
+        }
+        res = self.es.index(index="screeps-console", doc_type="error", body=body)
+        print res
 
     def process_cpu(self, ws, data):
+        body = {
+            'timestamp': datetime.now()
+        }
+
         if 'cpu' in data:
+            body['cpu'] = data['cpu']
             print data['cpu']
 
         if 'memory' in data:
+            body['memory'] = data['memory']
             print data['memory']
+
+        if 'cpu' in data or 'memory' in data:
+            res = self.es.index(index="screeps-performance", doc_type="performance", body=body)
+            print res
 
 
 

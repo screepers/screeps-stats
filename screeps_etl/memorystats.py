052c2bd9ce0cc5b48ed3c0ecb35912105f7ac5bb
Robert Hafner <tedivm@tedivm.com>
2016-06-10 16:35:57 -0700

    Fix console call to clear out stats data from the server

--- screeps_etl/memorystats.py
+++ screeps_etl/memorystats.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from elasticsearch import Elasticsearch
 import screepsapi
 from settings import getSettings
+import six
 import time
 
 class ScreepsMemoryStats():
@@ -59,14 +60,16 @@ class ScreepsMemoryStats():
                     res = self.es.index(index=indexname, doc_type="stats", body=groupstats)
                     print res
 
-
                 self.confirm(tick)
 
     def confirm(self, tick):
-        if(isinstance( tick, ( int, long ) )):
+
+        if isinstance(tick, six.string_types):
+            tick = int(tick)
+
+        if (isinstance( tick, int)) or (isinstance( tick, long)):
             sconn = self.getScreepsAPI()
-            'ScreepsStats.removeTick(' + str(tick) + ');'
-            javascript_clear = 'ScreepsStats.removeTick(' + str(tick) + ');'
+            javascript_clear = 'Stats.removeTick(' + str(tick) + ');'
             sconn.console(javascript_clear)
 
 

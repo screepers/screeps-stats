ae699a405a5e725b8025eb264ca7977e3c44c276
Robert Hafner <tedivm@tedivm.com>
2016-06-10 01:01:27 -0700

    apt mirrors and disabled ipv6 to speed up provisioning

--- provisioning/vagrant.sh
+++ provisioning/vagrant.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
+echo "** Adding Apt Mirrors **"
+echo "$(echo deb mirror://mirrors.ubuntu.com/mirrors.txt trusty main restricted universe multiverse | cat - todo.txt)" > /etc/apt/sources.list
+echo "$(echo deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-updates main restricted universe multiverse | cat - todo.txt)" > /etc/apt/sources.list
+echo "$(echo deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-backports main restricted universe multiverse | cat - todo.txt)" > /etc/apt/sources.list
+echo "$(echo deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-security main restricted universe multiverse | cat - todo.txt)" > /etc/apt/sources.list
+
+echo "** Disabling IPv6 **"
+echo 'precedence ::ffff:0:0/96 100' >> /etc/gai.conf
+echo "#disable ipv6" | tee -a /etc/sysctl.conf
+echo "net.ipv6.conf.all.disable_ipv6 = 1" | tee -a /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" | tee -a /etc/sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6 = 1" | tee -a /etc/sysctl.conf
+sysctl -p
+
 /vagrant/provisioning/provision.sh

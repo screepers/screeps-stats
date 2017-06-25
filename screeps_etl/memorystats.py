ca8fe80fca90105497ac233b06cc28ef021e2a81
Robert Hafner <tedivm@tedivm.com>
2017-06-24 20:59:51 -0700

    force market index mapping, import pages until no new records are found

--- screeps_etl/memorystats.py
+++ screeps_etl/memorystats.py
@@ -9,7 +9,10 @@ import six
 import time
 import os
 import services.screeps as screeps_service
+import sys
 
+MAXPAGES = 200
+es_settings_dir = os.path.join(os.path.dirname(__file__), 'mappings')
 
 class ScreepsMemoryStats():
 
@@ -30,76 +33,139 @@ class ScreepsMemoryStats():
     __api = False
 
     def run_forever(self):
+        lastrun = False
         while True:
+
             self.collectMemoryStats()
-            self.collectMarketHistory()
-            time.sleep(5)
+
+            # Market data changes much more rarely so process it less often.
+            if not lastrun or lastrun >= 20:
+                self.collectMarketHistory()
+                lastrun = 1
+                # don't pause before next run as market collection adds its own
+                # delays
+                continue
+
+            lastrun += 1
+            time.sleep(7)
 
     def collectMarketHistory(self):
         screeps = self.getScreepsAPI()
-        market_history = screeps.market_history()
+        page = None
+        failures = 0
 
-        if 'list' not in market_history:
-            return
+        while True:
 
-        for item in market_history['list']:
-            item['id'] = item['_id']
-            del item['_id']
-            if item['type'] == 'market.fee':
-                if 'extendOrder' in item['market']:
-                    item['addAmount'] = item['market']['extendOrder']['addAmount']
-                elif 'order' in item['market']:
-                    item['orderType'] = item['market']['order']['type']
-                    item['resourceType'] = item['market']['order']['resourceType']
-                    item['price'] = item['market']['order']['price']
-                    item['totalAmount'] = item['market']['order']['totalAmount']
-                    item['roomName'] = item['market']['order']['roomName']
-                else:
+            market_history = screeps.market_history(page)
+
+            if 'list' not in market_history:
+                return
+
+            for item in market_history['list']:
+                if '_id' not in item:
                     continue
-                self.saveFee(item)
-            else:
-                item['resourceType'] = item['market']['resourceType']
-                item['price'] = item['market']['price']
-                item['totalAmount'] = item['market']['amount']
-                if 'roomName' in item['market']:
-                    item['roomName'] = item['market']['roomName']
 
-                if 'targetRoomName' in item['market']:
-                    item['targetRoomName'] = item['market']['targetRoomName']
-                    user = screeps_service.getRoomOwner(item['targetRoomName'])
-                    if user:
-                        item['player'] = user
-                        alliance = screeps_service.getAllianceFromUser(user)
-                        if alliance:
-                            item['alliance'] = alliance
-
-                if 'npc' in item['market']:
-                    item['npc'] = item['market']['npc']
+                item['id'] = item['_id']
+                del item['_id']
+                if item['type'] == 'market.fee':
+                    if 'extendOrder' in item['market']:
+                        item['addAmount'] = item['market']['extendOrder']['addAmount']
+                    elif 'order' in item['market']:
+                        item['orderType'] = item['market']['order']['type']
+                        item['resourceType'] = item['market']['order']['resourceType']
+                        item['price'] = item['market']['order']['price']
+                        item['totalAmount'] = item['market']['order']['totalAmount']
+                        if 'roomName' in item['market']['order']:
+                            item['roomName'] = item['market']['order']['roomName']
+                    else:
+                        continue
+                    if self.saveFee(item):
+                        failures = 0
+                    else:
+                        failures += 1
                 else:
-                    item['npc'] = False
-                self.saveOrder(item)
+                    item['resourceType'] = item['market']['resourceType']
+                    item['price'] = item['market']['price']
+                    item['totalAmount'] = item['market']['amount']
+                    if 'roomName' in item['market']:
+                        item['roomName'] = item['market']['roomName']
+
+                    if 'targetRoomName' in item['market']:
+                        item['targetRoomName'] = item['market']['targetRoomName']
+                        user = screeps_service.getRoomOwner(item['targetRoomName'])
+                        if user:
+                            item['player'] = user
+                            alliance = screeps_service.getAllianceFromUser(user)
+                            if alliance:
+                                item['alliance'] = alliance
+
+                    if 'npc' in item['market']:
+                        item['npc'] = item['market']['npc']
+                    else:
+                        item['npc'] = False
+
+                    if self.saveOrder(item):
+                        failures = 0
+                    else:
+                        failures += 1
+
+            if failures >= 10:
+                print('Too many already captured records')
+                return
+
+            if 'hasMore' not in market_history:
+                print('hasMore not present')
+                return
+
+            if not market_history['hasMore']:
+                print('hasMore is false')
+                return
+
+            page = int(market_history['page']) + 1
+            if page >= MAXPAGES:
+                return
+
 
     def saveFee(self, order):
         date_index = time.strftime("%Y_%m")
         indexname = 'screeps-market-fees_' + date_index
-        if not self.es.exists(index=indexname, doc_type="orders", id=order['id']):
+
+        if not self.es.indices.exists(indexname):
+            with open('%s/fees.json' % (es_settings_dir,), 'r') as settings_file:
+                settings=settings_file.read()
+            self.es.indices.create(index=indexname, ignore=400, body=settings)
+
+        order = self.clean(order)
+        if self.es.exists(index=indexname, doc_type="fees", id=order['id']):
+            return False
+        else:
             self.es.index(index=indexname,
                           doc_type="fees",
                           id=order['id'],
                           timestamp=order['date'],
                           body=order)
+            print("Saving order (fee) %s" % (order['id'],))
+            return True
 
     def saveOrder(self, order):
         date_index = time.strftime("%Y_%m")
         indexname = 'screeps-market-orders_' + date_index
+        if not self.es.indices.exists(indexname):
+            with open('%s/orders.json' % (es_settings_dir,), 'r') as settings_file:
+                settings=settings_file.read()
+            self.es.indices.create(index=indexname, ignore=400, body=settings)
 
-        if not self.es.exists(index=indexname, doc_type="orders", id=order['id']):
+        order = self.clean(order)
+        if self.es.exists(index=indexname, doc_type="orders", id=order['id']):
+            return False
+        else:
             self.es.index(index=indexname,
                           doc_type="orders",
                           id=order['id'],
                           timestamp=order['date'],
                           body=order)
-
+            print("Saving order (deal) %s" % (order['id'],))
+            return True
 
 
     def collectMemoryStats(self):
@@ -169,10 +235,13 @@ class ScreepsMemoryStats():
     def clean(self, datadict):
         newdict = {}
         for key, value in datadict.iteritems():
-            try:
-                newdict[key] = float(value)
-            except:
-                newdict[key] = value
+            if key == 'tick':
+                newdict[key] = int(value)
+            else:
+                try:
+                    newdict[key] = float(value)
+                except:
+                    newdict[key] = value
         return datadict
 
 
